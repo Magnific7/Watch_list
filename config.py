c40a3a57ac7393c0c1c535a3afd08fe065e036b4
Magnific7 <mjones6944@gmail.com>
2019-09-18 16:22:49 +0200

    sending email message after sign in

--- config.py
+++ config.py
@@ -6,7 +6,14 @@ class Config:
     MOVIE_API_KEY = os.environ.get('MOVIE_API_KEY')
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://wecode:wecode@localhost/watchlist'
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
     SQLALCHEMY_TRACK_MODIFICATIONS = True
+    #  email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
 
     @staticmethod
     def init_app(app):
--- start.sh
+++ start.sh
@@ -1,5 +1,7 @@
 export MOVIE_API_KEY='58aed921d71acba6bbb12787dbef553c'
 export SECRET_KEY='<Flask WTF Secret Key>'
+export MAIL_USERNAME='mjones6944@gmail.com'
+export MAIL_PASSWORD='2015s3red'
 
 python3.6 manage.py server
 
